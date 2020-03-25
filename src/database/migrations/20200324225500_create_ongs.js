3f6d016a7e7fb08719371f031c1e201db41540ff
Karol Harumi <karol.harumi@pebmed.com.br>
2020-03-24 23:05:54 -0600

    SQLite config and create tables

---
+++ src/database/migrations/20200324225500_create_ongs.js
@@ -0,0 +1,16 @@
+
+exports.up = function(knex) {
+    return knex.schema.createTable('ongs', function(table){
+        table.string('id').primary();
+        table.string('name').notNullable();
+        table.string('email').notNullable();
+        table.string('whatsapp').notNullable();
+        table.string('city').notNullable();
+        table.string('uf', 2).notNullable();
+    });
+};
+
+// caso de algum problema o que preciso fazer? deletar a tabela nesse caso
+exports.down = function(knex) {
+  return knex.schema.dropTable('ongs');
+};
---
+++ src/database/migrations/20200324230157_create_incidents.js
@@ -0,0 +1,19 @@
+
+exports.up = function(knex) {
+    return knex.schema.createTable('incidents', function(table){
+        table.increments();
+
+        table.string('title').notNullable();
+        table.string('description').notNullable();
+        table.decimal('value').notNullable();
+
+        table.string('ong_id').notNullable();
+
+        table.foreign('ong_id').references('id').inTable('ongs');
+    });
+};
+
+// caso de algum problema o que preciso fazer? deletar a tabela nesse caso
+exports.down = function(knex) {
+  return knex.schema.dropTable('incidents');
+};
