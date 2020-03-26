757a595843617a5a8f74fcde1542e443aa854136
Karol Harumi <karol.harumi@pebmed.com.br>
2020-03-25 21:37:59 -0600

    Add cors package

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const cors = require('cors');
 const routes = require('./routes');
 
 const app = express();
 
+app.use(cors()); //to allow all frontend applications to access this backend
 app.use(express.json());
 app.use(routes);
 
