3bec9df85094bb35b9cb42e6f8cd5d55b8e9a6d5
Karol Harumi <karol.harumi@pebmed.com.br>
2020-03-28 17:27:33 -0600

    Integrating logon with api

--- frontend/src/pages/Logon/index.js
+++ frontend/src/pages/Logon/index.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { FiLogIn } from 'react-icons/fi';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
+import api from '../../services/api';
 
 import './style.css';
 
@@ -8,15 +9,37 @@ import logoImg from '../../assets/logo.svg';
 import heroesImg from '../../assets/heroes.png';
 
 export default function Logon() {
+    const [id, setId] = useState('');
+
+    const history = useHistory();
+
+    async function handleLogon(e) {
+        e.preventDefault();
+
+        try{
+            const response = await api.post("sessions", { id });
+
+            localStorage.setItem("ongId", id);
+            localStorage.setItem("ongName", response.data.name);
+            history.push('/profile');
+        } catch(err) {
+            alert("Erro ao logar, tente novamente.");
+        }
+    }
+
     return (
         <div className="logon-container">
             <section className="form">
                 <img src={logoImg} alt={"Be the Hero"} />
 
-                <form>
+                <form onSubmit={handleLogon}>
                     <h1>Faça seu logon</h1>
 
-                    <input placeholder="Seu ID"/>
+                    <input 
+                        placeholder="Seu ID"
+                        value={id}
+                        onChange={e => setId(e.target.value)}
+                    />
                     <button type="submit" className="button">Entrar</button>
 
                     <Link to="/register" className="back-link">
