a13a3e8e7b5bc3f1b4a69a1e8c7f0d73f1ccbd21
Karol Harumi <karol.harumi@pebmed.com.br>
2020-03-28 18:05:18 -0600

    Integrating incidents register with api

--- frontend/src/pages/Profile/index.js
+++ frontend/src/pages/Profile/index.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { FiPower, FiTrash2 } from 'react-icons/fi';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 
 import './style.css';
 
@@ -9,6 +9,8 @@ import api from '../../services/api';
 
 export default function Profile() {
     const [incidents, setIncidents] = useState([]);
+
+    const history = useHistory();
 
     const ongId = localStorage.getItem('ongId');
     const ongName = localStorage.getItem('ongName');
@@ -37,6 +39,11 @@ export default function Profile() {
         }
     }
 
+    function handleLogout() {
+        localStorage.clear();
+        history.push('/');
+    }
+
     return(
         <div className="profile-container">
             <header>
@@ -44,7 +51,7 @@ export default function Profile() {
                 <span>Bem-vinda, {ongName}</span>
 
                 <Link className="button" to="/incidents/new">Cadastrar novo caso</Link>
-                <button type="button">
+                <button onClick={handleLogout} type="button">
                     <FiPower size={18} color={'#e02041'}></FiPower>
                 </button>
             </header>
