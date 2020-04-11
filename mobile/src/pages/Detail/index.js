6ae28354f1ddb70122eae0dfe60caa9f0fcc3f63
Karol Harumi <karol.harumi@pebmed.com.br>
2020-04-10 22:00:22 -0600

    Config mobile routes

---
+++ mobile/src/pages/Detail/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View } from 'react-native';
+
+import styles from './styles'
+
+export default function Detail() {
+    return(
+
+        <View>
+            
+        </View>
+
+    )
+}
